94a62236d80ab3801d5bdf3fcc687549df8b930f
Ganna Simonova <anya_simonova@yahoo.com>
2019-04-01 00:16:29 +0300

    Finised layout for "For all" block, "Baby side" block

--- js/action.js
+++ js/action.js
@@ -1,32 +1,49 @@
 "use strict";
 
+const burgerMenu = document.querySelector(".burger_menu");
+const menuList = document.querySelector(".menu_list");
+const menuItems = document.querySelector(".menu_items");
+const blurredPart = document.querySelector(".blurred");
+
 document.body.onload = function() {
     toggleHeaderMenuItems();
+    toggleSideMenu();
     chooseSortType();
 };
 
 function toggleHeaderMenuItems() {
-    let menuItems = document.querySelectorAll("header ul li a");
+    let menuItems = document.querySelectorAll(".header_list li a");
     menuItems.forEach(function(item) {
         item.addEventListener('click', function () {
-            item.classList.add("active-elem");
+            item.classList.add("header-menu-active");
             menuItems.forEach(function(value) {
                 if (value !== item) {
-                    value.classList.remove("active-elem");
+                    value.classList.remove("header-menu-active");
                 }
             });
         });
     });
 }
 
-function toggleSideMenu(targetBlockName) {
-    // toggle target menu block
-    let targetBlock = document.getElementById(targetBlockName);
-    let burgerMenu = targetBlock.querySelector(".burger_menu");
-    let menuList = targetBlock.querySelector(".menu_list");
+function toggleSideMenu() {
+    burgerMenu.addEventListener('click', function() {
+        if (burgerMenu.classList.contains('burger_menu_active')) {
+            burgerMenu.classList.remove('burger_menu_active');
+            menuList.classList.add("hidden");
+            menuItems.classList.add("menu_items_hidden");
+            blurredPart.classList.add("hidden");
+        } else {
+            burgerMenu.classList.add('burger_menu_active');
+            menuList.classList.remove("hidden");
+            menuItems.classList.remove("menu_items_hidden");
+            menuItems.addEventListener("transitionend", blurBackground());
+        }
+    });
+}
 
-    burgerMenu.classList.contains('active-elem') ? burgerMenu.classList.remove('active-elem') : burgerMenu.classList.add('active-elem');
-    menuList.classList.toggle("hidden");
+function blurBackground() {
+    blurredPart.classList.remove("hidden");
+    // menuItems.removeEventListener("transitionend", blurBackground);
 }
 
 function chooseSortType() {
